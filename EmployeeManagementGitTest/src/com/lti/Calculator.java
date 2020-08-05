dc5afcfc7f6aa4e4503b096fc3d5478452e3f82b
abc <abc@DESKTOP-SQ54JI2>
2020-08-05 15:49:30 +0530

    Added Files through eclipse

---
+++ EmployeeManagementGitTest/src/com/lti/Calculator.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+public class Calculator {
+	
+	public static void add(int a, int b) {
+	    System.out.println("Sum = " + (a+b));
+	  }
+
+	public static void main(String[] args) {
+		add(10, 30);
+
+	}
+
+}
